d4e547fb77264d88778187ae91700f7a9e034df0
Javlonbek <sizning@email.com>
2026-06-05 14:40:26 +0500

    fix: use process.env.PORT

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const parser = require('./parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
